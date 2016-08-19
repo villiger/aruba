d0b6e45dc223f4dc6f2879e994336e16cf0a499b
Andy Villiger <a.villiger@gmail.com>
2016-08-19 20:00:54 +0100

    initial commit

---
+++ pages/arbeiten.php
@@ -0,0 +1,25 @@
+<div id="slider" class="slider">
+    <img src="/images/arbeiten/01_sonvilier_aussenfoto.jpg" alt="Studienauftrag Wohnheim, Sonvilier, mit ds.architekten">
+    <img data-src="/images/arbeiten/02_sonvilier_grundriss_og_weiss.png" src="" alt="Studienauftrag Wohnheim, Sonvilier, mit ds.architekten">
+    <img data-src="/images/arbeiten/03_cabaret_maison.jpg" src="" alt="Wiederaufbau von Privathäusern, Cabaret, Caritas Schweiz, Haiti">
+    <img data-src="/images/arbeiten/04_cabaret_construction.jpg" src="" alt="Wiederaufbau von Privathäusern, Cabaret, Caritas Schweiz, Haiti">
+    <img data-src="/images/arbeiten/05_baloise_cover.jpg" src="" alt="Vorbereitung und Begleitung Studienauftrag, Basler Versicherungen, mit Beatrice Bayer Architekten">
+    <img data-src="/images/arbeiten/06_baloise_inhalt.jpg" src="" alt="Vorbereitung und Begleitung Studienauftrag, Basler Versicherungen, mit Beatrice Bayer Architekten">
+    <img data-src="/images/arbeiten/07_fehraltorf_aussen.jpg" src="" alt="Wettbewerb Kindertagesstätte, Fehraltorf, mit Architheke AG">
+    <img data-src="/images/arbeiten/08_fehraltorf_innen.jpg" src="" alt="Wettbewerb Kindertagesstätte, Fehraltorf, mit Architheke AG">
+    <img data-src="/images/arbeiten/09_haiti_darbonne.jpg" src="" alt="Wiederaufbau von Schulzentren, Darbonne, Caritas Schweiz, Haiti">
+    <img data-src="/images/arbeiten/10_haiti_gressier.jpg" src="" alt="Wiederaufbau von Schulzentren, Darbonne, Caritas Schweiz, Haiti">
+    <img data-src="/images/arbeiten/11_pilotnuntzung_herleitung.jpg" src="" alt="Theoriearbeit Pilotnutzung, FHNW, mit Philippe Guillod">
+    <img data-src="/images/arbeiten/12_pilotnutzung_in_ketten.jpg" src="" alt="Theoriearbeit Pilotnutzung, FHNW, mit Philippe Guillod">
+</div>
+<script>
+    $(document).ready(function() {
+        var slider = new IdealImageSlider.Slider({
+            selector: '#slider',
+            height: 500,
+            interval: 20000
+        });
+        slider.addCaptions();
+        slider.start();
+    });
+</script>
